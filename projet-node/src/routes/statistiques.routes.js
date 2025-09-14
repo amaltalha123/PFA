0500d73d98b543e2c1cf9edc461d093c85079604
amaltalha123 <talhaamal204@gmail.com>
2025-09-14 12:11:50 +0200

    commit 1

--- projet-node/src/routes/statistiques.routes.js
+++ projet-node/src/routes/statistiques.routes.js
@@ -3,8 +3,7 @@ const router = express.Router();
 const {getTotalStagiaires,getTotalEncadrant,getTotalActualStage,getTotalfinishedStage,getTotalEncadrantOwnStage,StageByType,getTotalStagiaireOwnStages,getNonCommentedTicketsNumber, getIncompleteMissions} = require('../controllers/statistiques.controller');
 const authMiddleware = require('../middleware/auth.middleware');
 const roleMiddleware = require('../middleware/role.middleware');
-const verifyOwnershipOr = require('../middleware/verifyOwnershipOr.middleware');
-const { Stage ,Stagiaire,ticket,Encadrant } = require('../models');
+
 
 router.get('/statistic/TotalStagiaires',authMiddleware,roleMiddleware('admin'), getTotalStagiaires);
 router.get('/statistic/TotalEncadrant',authMiddleware,roleMiddleware('admin'), getTotalEncadrant);
@@ -12,7 +11,5 @@ router.get('/statistic/finishedStage',authMiddleware,roleMiddleware('admin'), ge
 router.get('/statistic/encadrant/toutMesStages',authMiddleware,roleMiddleware('encadrant'),getTotalEncadrantOwnStage);
 router.get('/statistic/StageByType',authMiddleware,roleMiddleware('admin'),StageByType);
 router.get('/statistic/stagiaire/toutMesStages',authMiddleware,roleMiddleware('stagiaire'),getTotalStagiaireOwnStages);
-router.get('/tickets/nonCommented/:id',authMiddleware,roleMiddleware('encadrant','stagiaire'),verifyOwnershipOr,getNonCommentedTicketsNumber);
-router.get('/missions/incompletes/:id',authMiddleware,roleMiddleware('encadrant','stagiaire'),verifyOwnershipOr,getIncompleteMissions);
 
 module.exports = router;
